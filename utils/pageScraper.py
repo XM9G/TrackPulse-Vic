ed5c2b8ce337d355ab155aea4581249119d452f9
AshKmo <74221118+AshKmo@users.noreply.github.com>
2024-09-07 21:36:18 +1000

    grab the chumps json directly from github

--- utils/pageScraper.py
+++ utils/pageScraper.py
@@ -1,70 +1,40 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import queue
-
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-from bs4 import BeautifulSoup
-import requests
-
 def transportVicSearch(search, tram=False):
-    if tram:
-        url = f'https://vic.transportsg.me/tram/tracker/fleet?fleet={search}'
-    else:
-        url = f'https://vic.transportsg.me/metro/tracker/consist?consist={search}'
+	if tram:
+		url = f'https://vic.transportsg.me/tram/tracker/fleet?fleet={search}'
+	else:
+		url = f'https://vic.transportsg.me/metro/tracker/consist?consist={search}'
 
-    try:
-        res = requests.get(url).text
+	try:
+		res = requests.get(url).text
 
-        soup = BeautifulSoup(res, features="lxml")
+		soup = BeautifulSoup(res, features="lxml")
 
-        # Find all divs with class "trip" but not "trip inactive"
-        elements = soup.find_all('div', class_='trip')
-        active_elements = [e for e in elements if 'inactive' not in e.get('class', [])]
+		elements = soup.find_all('div', class_="trip")
 
-        if not active_elements:
-            return ["Error: No active trips found. Train may be invalid or not currently running."]
+		if not elements:
+			return ["Error: No trips found. Train may be invalid or not currently running."]
 
-        trip_texts = [element.text for element in active_elements]
+		trip_texts = [element.text for element in elements]
 
-        return trip_texts
-    except Exception as e:
-        return [f'Error: {e}']
-
+		return trip_texts
+	except Exception as e:
+		return [f'Error: {e}']
 
 def TRAMtransportVicSearch(search, tram=True):
 	return transportVicSearch(search, tram=tram)
 
-def montagueDays(queue):
-	service = Service(ChromeDriverManager().install())
-	driver = webdriver.Chrome(service=service)
-
-	url = f'https://howmanydayssincemontaguestreetbridgehasbeenhit.com'
-
-	# Open the URL in the browser
-	driver.get(url)
+def montagueDays(q):
+	url = "https://raw.githubusercontent.com/TeeWallz/monty_balboa/main/public/chumps.json"
 
 	try:
-		# Wait for the page to load
-		driver.implicitly_wait(5)
+		chumps = requests.get(url).json()
 
-		# Find all elements with class
-		elements = driver.find_element_by_class_name('jss25')
+		days = (datetime.now() - datetime.strptime(chumps[0]["date"], "%Y-%m-%d")).days
 
-		if elements:
-			days = elements.text
-			print(days)
-			queue.put(days)
-		else:
-			return("`Error: Number not found`")
+		q.put(str(days))
 	except Exception as e:
-		return(f'Error: {e}')
-	finally:
-		driver.quit()
+		return [f'Error: {e}']
